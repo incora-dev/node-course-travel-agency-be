202ceb6f22cfa8d183795f5ae5603bff6a5ba83c
lobairoman <lobairoman@gmail.com>
2019-09-05 13:44:18 +0300

    Get all users implemented + documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
-import {UsersService} from './users.service';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { IUser } from './interfaces/user.interface';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) { }
 
+    @Get()
+    @ApiResponse({ status: 200, description: 'List of Users' })
+    async getAll(): Promise<IUser[]> {
+        return await this.usersService.getAllFromDB();
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
-import {Repository} from 'typeorm';
-import {User} from './user.entity';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 import { IUser } from './interfaces/user.interface';
 
 @Injectable()
@@ -8,6 +8,9 @@ export class UsersService {
     constructor(
         @Inject('USER_REPOSITORY')
         private readonly userRepository: Repository<User>,
-    ) {}
+    ) { }
 
+    async getAllFromDB(): Promise<IUser[]> {
+        return await this.userRepository.find();
+    }
 }
