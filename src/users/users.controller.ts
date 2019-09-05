feeeefeb9d7e86dbe92e11322b657c6c0a4b356e
YKhemych <yosyp0108khemych@gmail.com>
2019-09-05 13:09:42 +0300

    Added endpoint getOneById and documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param, UseInterceptors, ClassSerializerInterceptor} from '@nestjs/common';
 import {UsersService} from './users.service';
+import {IUser} from './interfaces/user.interface';
+import {ApiImplicitQuery, ApiResponse, ApiImplicitParam} from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @Get(':id')
+    @ApiImplicitParam({ name: 'id', type: Number })
+    @ApiResponse({ status: 200, description: 'User Object'})
+    @ApiResponse({ status: 404, description: 'Error Exception ```{ statusCode: 404, message: "Not found" }```' })
+    getOneById(@Param() params): Promise<IUser> {
+        return this.usersService.getOneById(params.id);
+    }
 }
