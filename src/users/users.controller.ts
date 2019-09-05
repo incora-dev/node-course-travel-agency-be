01a398a9203c973c9b2ffc006ec2006c8e99053b
YKhemych <yosyp0108khemych@gmail.com>
2019-09-05 14:38:08 +0300

    Added endpoint deleteUser and documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Delete, Param} from '@nestjs/common';
 import {UsersService} from './users.service';
+import {IUser} from './interfaces/user.interface';
+import {ApiImplicitParam, ApiResponse} from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @Delete(':id')
+    @ApiImplicitParam({ name: 'id', type: Number })
+    @ApiResponse({ status: 200, description: 'User Object ```deleted User()```' })
+    @ApiResponse({ status: 404, description: 'Error Exception ```{ statusCode: 404, message: "Not found" }```' })
+    deleteUserById(@Param() params): Promise<IUser> {
+        return this.usersService.deleteUserById(params.id);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import {Injectable, Inject, HttpException, HttpStatus} from '@nestjs/common';
 import {Repository} from 'typeorm';
 import {User} from './user.entity';
 import { IUser } from './interfaces/user.interface';
@@ -10,4 +10,11 @@ export class UsersService {
         private readonly userRepository: Repository<User>,
     ) {}
 
+    async deleteUserById(id: number): Promise<IUser> {
+        const user = await this.userRepository.findOne(id);
+        if (!user) {
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
+        return await this.userRepository.remove(user);
+    }
 }
