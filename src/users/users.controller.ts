1328bea713eeaa24eb2597dfcae3ad27b2aa83b0
YKhemych <yosyp0108khemych@gmail.com>
2019-09-05 13:55:27 +0300

    Added endpoint updateUser and documentation, updateUserDto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Param, Put} from '@nestjs/common';
 import {UsersService} from './users.service';
+import {UpdateUserDto} from './dto/user.dto';
+import {ApiResponse, ApiImplicitParam} from '@nestjs/swagger';
+import {IUser} from './interfaces/user.interface';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @Put(':id')
+    @ApiImplicitParam({ name: 'id', type: Number })
+    @ApiResponse({ status: 200, description: 'User Object ```new User()```' })
+    @ApiResponse({ status: 404, description: 'Error Exception ```{ statusCode: 404, message: "Not found" }```' })
+    updateUser(@Param() params, @Body() user: UpdateUserDto): Promise<IUser> {
+        return this.usersService.updateUser(params.id, user);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Inject } from '@nestjs/common';
+import {Injectable, Inject, HttpException, HttpStatus} from '@nestjs/common';
 import {Repository} from 'typeorm';
 import {User} from './user.entity';
 import { IUser } from './interfaces/user.interface';
+import {UpdateUserDto} from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
@@ -10,4 +11,11 @@ export class UsersService {
         private readonly userRepository: Repository<User>,
     ) {}
 
+    async updateUser(id: number, data: UpdateUserDto ): Promise<IUser> {
+        const user = await this.userRepository.findOne(id);
+        if (!user) {
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
+        return await this.userRepository.save({ ...data, id: Number(id) });
+    }
 }
