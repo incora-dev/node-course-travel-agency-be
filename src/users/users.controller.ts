cabf544b87c365ab38a451e451b4d8e147f086af
lobairoman <lobairoman@gmail.com>
2019-09-06 18:23:31 +0300

    Post users implemented + documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,22 @@
-import { Controller } from '@nestjs/common';
-import {UsersService} from './users.service';
+import { Controller, Post, Body, HttpException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { IUser } from './interfaces/user.interface';
+import { ApiResponse } from '@nestjs/swagger';
+import { UserDTO } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) { }
 
+    @Post()
+    @ApiResponse({ status: 201, description: 'The User has been successfully added.' })
+    @ApiResponse({ status: 404, description: 'Error Exception ```{ statusCode: 400, message: "User exists!" }```' })
+    async add(@Body() user: UserDTO): Promise<IUser> {
+        const email = user.email;
+        const userFromDB = await this.usersService.getOneByParams({ email });
+        if (userFromDB) {
+            throw new HttpException('User exists!', 400);
+        }
+        return await this.usersService.addToDB(user);
+    }
 }
