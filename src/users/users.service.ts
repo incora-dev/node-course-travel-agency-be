877fa93c63e72d7ede9d51b507164cfb65f791b7
Dmytro Roik <dmytro.roik@incorainc.com>
2019-09-07 22:08:37 +0300

    hotifx for creating user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -37,9 +37,9 @@ export class UsersService {
 
     async getOneByParams(params: object): Promise<IUser> {
         const user = await this.userRepository.findOne(params);
-        if (!user) {
-            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
-        }
+        // if (!user) {
+            // throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        // }
         return user;
     }
 }
