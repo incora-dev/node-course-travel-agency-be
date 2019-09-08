67713ac155c0988d68f73f97cfa1954c656264c9
YKhemych <yosyp0108khemych@gmail.com>
2019-09-08 22:27:07 +0300

    Added middleware to check if a tour exist

--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -1,12 +1,23 @@
-import { Module } from '@nestjs/common';
+import {Module, MiddlewareConsumer, RequestMethod} from '@nestjs/common';
 import { ToursService } from './tours.service';
 import { ToursController } from './tours.controller';
 import {DatabaseModule} from '../core/database.module';
 import {tourProviders} from './tour.providers';
+import {TourIsExistMiddleware} from 'src/middlewares/tour.isExist.middleware';
 
 @Module({
   imports: [DatabaseModule],
   providers: [ToursService, ...tourProviders],
   controllers: [ToursController],
 })
-export class ToursModule {}
+export class ToursModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+        .apply(TourIsExistMiddleware)
+        .forRoutes(
+            {path: 'tours/:id', method: RequestMethod.GET},
+            {path: 'tours/:id', method: RequestMethod.PUT},
+            {path: 'tours/:id', method: RequestMethod.DELETE},
+        );
+  }
+}
