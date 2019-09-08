ec185e5f9197ca4c5153d235af3c407b870c3b6c
YKhemych <yosyp0108khemych@gmail.com>
2019-09-08 22:19:59 +0300

    Added getOne tour method and documentation

--- src/tours/tours.controller.ts
+++ src/tours/tours.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
+import {ApiUseTags, ApiImplicitParam, ApiResponse} from '@nestjs/swagger';
+import {ToursService} from './tours.service';
+import {ITour} from './interface/tour.interface';
 
+@ApiUseTags('tours')
 @Controller('tours')
-export class ToursController {}
+export class ToursController {
+    constructor(private readonly toursService: ToursService) {}
+
+    @Get(':id')
+    @ApiImplicitParam({ name: 'id', type: Number })
+    @ApiResponse({ status: 200, description: 'Ok ```Tour Object```'})
+    @ApiResponse({ status: 404, description: 'Error Exception ```{ statusCode: 404, message: "Not found" }```' })
+    getOneById(@Param('id') id: number): Promise<ITour> {
+        return this.toursService.getOneByParams({ id });
+    }
+}
--- src/tours/tours.service.ts
+++ src/tours/tours.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {Repository} from 'typeorm';
+import {Tour} from './tour.entity';
+import {ITour} from './interface/tour.interface';
 
 @Injectable()
-export class ToursService {}
+export class ToursService {
+    constructor(
+        @Inject('TOUR_REPOSITORY')
+        private readonly tourRepository: Repository<Tour>,
+    ) {}
+
+    async getOneByParams(params: object): Promise<ITour> {
+        return await this.tourRepository.findOne(params);
+    }
+}
