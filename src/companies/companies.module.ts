a058d5e57b080bc461456fcf87db5054a47f2df7
YKhemych <yosyp0108khemych@gmail.com>
2019-09-08 14:56:42 +0300

    Middleware placement has been fixed

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,7 +3,7 @@ import {CompaniesService} from './companies.service';
 import {CompaniesController} from './companies.controller';
 import {DatabaseModule} from '../core/database.module';
 import {companyProviders} from './company.providers';
-import {CompanyIsExistMiddleware} from './middlewares/company.isExist.middleware';
+import {CompanyIsExistMiddleware} from '../middlewares/company.isExist.middleware';
 
 @Module({
   imports: [DatabaseModule],
