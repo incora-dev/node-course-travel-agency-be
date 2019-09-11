cea3d2eecaa01c4b2eb73d816730a6c80cc20b7e
YKhemych <yosyp0108khemych@gmail.com>
2019-09-11 22:49:41 +0300

    Fixed tours dto, interface, entity

--- src/tours/interface/tour.interface.ts
+++ src/tours/interface/tour.interface.ts
@@ -1,9 +1,6 @@
-import {RoomType} from '../enums/roomType.enum';
 
 export interface ITour {
     _id?: number;
-    price: number;
-    roomType: RoomType;
     startDate: Date;
     endDate: Date;
     description: string;
--- src/tours/tour.entity.ts
+++ src/tours/tour.entity.ts
@@ -1,16 +1,9 @@
 import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
-import {RoomType} from './enums/roomType.enum';
 
 @Entity()
 export class Tour {
     @PrimaryGeneratedColumn()
     id: number;
-
-    @Column()
-    price: number;
-
-    @Column('text')
-    roomType: RoomType;
 
     @Column('date')
     startDate: Date;
