87611a468aba06a60d168553c45310789fdf56d3
YKhemych <yosyp0108khemych@gmail.com>
2019-09-12 11:49:09 +0300

    Create connection to DB, dto, interface, enum

---
+++ src/rooms/interfaces/room.interface.ts
@@ -0,0 +1,7 @@
+import {RoomType} from '../enums/roomType.enum';
+
+export interface IRoom {
+    _id?: number;
+    price: number;
+    roomType: RoomType;
+}
--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import {DatabaseModule} from 'src/core/database.module';
+import {roomProviders} from './room.providers';
 
 @Module({
-  providers: [RoomsService],
+  imports: [DatabaseModule],
+  providers: [RoomsService, ...roomProviders],
   controllers: [RoomsController],
 })
 export class RoomsModule {}
