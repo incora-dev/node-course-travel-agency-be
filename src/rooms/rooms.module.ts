b2499b7d749f68e62ba06418ccbc48b3e0682846
YKhemych <yosyp0108khemych@gmail.com>
2019-09-12 13:44:53 +0300

    Added room is exist middleware

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, RequestMethod} from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
 import {roomProviders} from './room.providers';
 import {tourProviders} from '../tours/tour.providers';
 import {DatabaseModule} from '../core/database.module';
+import {RoomIsExistMiddleware} from '../middlewares/room.isExist.middleware';
 
 @Module({
   imports: [DatabaseModule],
@@ -13,4 +14,14 @@ import {DatabaseModule} from '../core/database.module';
       ...tourProviders],
   controllers: [RoomsController],
 })
-export class RoomsModule {}
+export class RoomsModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(RoomIsExistMiddleware)
+            .forRoutes(
+                {path: 'rooms/:id', method: RequestMethod.GET},
+                {path: 'rooms/:id', method: RequestMethod.PUT},
+                {path: 'rooms/:id', method: RequestMethod.DELETE},
+            );
+    }
+}
